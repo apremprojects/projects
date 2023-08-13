7f44c676b9599686af7ea1c83483b045f51555e3
apremprojects <arjundprem@outlook.com>
2023-08-12 21:49:37 -0700

    Add raw imu read arduino project

---
+++ arduino/altimu_v10_raw_data/alt_imu_raw_data.ino
@@ -0,0 +1,97 @@
+#include <Wire.h>
+#include <LPS.h>
+#include <LSM6.h>
+#include <LIS3MDL.h>
+
+LPS ps;
+LSM6 imu;
+LIS3MDL mag;
+
+enum Mode { TRANSMIT, STOP };
+
+Mode mode;
+char report[1024];
+unsigned long int prev_time;
+unsigned long int heart_beat_time;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  Wire.begin();
+
+  while (!imu.init()) {
+      Serial.println(F("Failed to detect the LSM6."));
+      delay(100);
+  }
+
+  imu.enableDefault();
+
+  // Set the gyro full scale to 1000 dps because the default
+  // value is too low, and leave the other settings the same.
+  imu.writeReg(LSM6::CTRL2_G, 0b10001000);
+
+  // Set the accelerometer full scale to 16 g because the default
+  // value is too low, and leave the other settings the same.
+  imu.writeReg(LSM6::CTRL1_XL, 0b10000100);
+
+  while (!mag.init()) {
+      // Failed to detect the LIS3MDL.
+      Serial.println(F("Failed to detect the LIS3MDL."));
+      delay(100);
+
+  }
+  mag.enableDefault();
+
+  while (!ps.init()) {
+      Serial.println("Failed to autodetect pressure sensor!");
+      delay(100);
+  }
+  ps.enableDefault();
+
+  Serial.println("Starting...");
+
+  mode = STOP;
+  prev_time = millis();
+}
+
+void loop() {
+  if (Serial.available()) {
+    char cmd = Serial.read();
+    switch (cmd) {
+      case 's':
+        mode = TRANSMIT;
+        heart_beat_time = millis();
+        break;
+    }
+  }
+
+  if (mode == STOP) {
+    return;
+  }
+
+  const unsigned long int curr_time = millis();
+  const unsigned int elapsed = curr_time - prev_time;
+  if (elapsed < 25) {
+    return;
+  }
+  prev_time = curr_time;
+  if ((curr_time - heart_beat_time) > 1000) {
+    mode = STOP;
+    return;
+  }
+
+  if (mode == TRANSMIT) {
+    imu.read();
+    mag.read();
+    const float temp = ps.readTemperatureF();
+    const float press = ps.readPressureInchesHg();
+    const float alt = ps.pressureToAltitudeFeet(press);
+    sprintf(report, "s: %d, %d, %d, %d, %d, %d, %d, %d, %d, %f, %f, %f, %lu\r\n", 
+      imu.a.x, imu.a.y, imu.a.z, 
+      imu.g.x, imu.g.y, imu.g.z, 
+      mag.m.x, mag.m.y, mag.m.z, 
+      press, alt, temp, curr_time);
+    Serial.print(report);
+  }
+}
