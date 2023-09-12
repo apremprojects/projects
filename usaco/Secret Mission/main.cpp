396cf423bfc7f3a390896b0711c41f4a9a9ff3e4
arjun p <arjundprem@outlook.com>
2023-09-12 07:16:11 -0700

    Secret Mission

---
+++ usaco/Secret Mission/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <vector>
+#include <array>
+#include <fstream>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+size_t char_to_int(const char c) {
+    return c - 'A' + 1;
+}
+
+size_t get_code(const string &s) {
+    size_t rv = 1;
+    for (const char c : s) {
+        rv *= char_to_int(c);
+    }
+    return rv % 47;
+}
+
+int main() {
+    string cow;
+    string code;
+    cin >> cow;
+    cin >> code;
+    if (get_code(cow) != get_code(code)) {
+        cout << "STAY\n";
+    }
+    else {
+        cout << "GO\n";
+    }
+}
