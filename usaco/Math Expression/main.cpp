70c14fc6fc7eadeaa17b7717f3422e3c4312b25b
apremprojects <arjundprem@outlook.com>
2023-09-23 09:32:01 -0700

    Added 2 more problems

---
+++ usaco/Math Expression/main.cpp
@@ -0,0 +1,67 @@
+﻿#include <string>
+#include <vector>
+#include <array>
+#include <fstream>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> Ss;
+    vector<int> Os;
+    vector<int> Ms;
+    vector<int> Es;
+    vector<int> Bs;
+    vector<int> Is;
+    vector<int> Gs;
+    for (int i = 0; i < N; i++) {
+        char c;
+        cin >> c;
+        int num;
+        cin >> num;
+        if (c == 'S') {
+            Ss.push_back(num);
+        }
+        else if (c == 'O') {
+            Os.push_back(num);
+        }
+        else if (c == 'M') {
+            Ms.push_back(num);
+        }
+        else if (c == 'E') {
+            Es.push_back(num);
+        }
+        else if (c == 'B') {
+            Bs.push_back(num);
+        }
+        else if (c == 'I') {
+            Is.push_back(num);
+        }
+        else if (c == 'G') {
+            Gs.push_back(num);
+        }
+    }
+    int count = 0;
+    for(const int S: Ss){
+        for (const int O : Os) {
+            for (const int M : Ms) {
+                for (const int E : Es) {
+                    for (const int B : Bs) {
+                        for (const int I : Is) {
+                            for (const int G : Gs) {
+                                int num = (O + M + O) * (S + I + S + B + E + E) * (E + G + O + S);
+                                if (num % 2 == 0) {
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << count << "\n";
+}
---
+++ usaco/Spring Cleaning/main.cpp
@@ -0,0 +1,65 @@
+﻿#include <string>
+#include <vector>
+#include <array>
+#include <fstream>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+struct squirrel_t {
+    int C;
+    bool start;
+    int HH;
+    int MM;
+};
+
+int operator-(const squirrel_t& a, const squirrel_t& b) {
+    int at = (a.HH * 60) + a.MM;
+    int bt = (b.HH * 60) + b.MM;
+    return at - bt;
+}
+
+bool operator<(const squirrel_t& a, const squirrel_t& b) {
+    if (a.HH < b.HH) {
+        return true;
+    }
+    else if (a.HH == b.HH) {
+        return a.MM < b.MM;
+    }
+    return false;
+}
+
+int cleaning_time(const vector<squirrel_t>& b) {
+    int time = 0;
+    for (int i = 0; i < b.size(); i += 2) {
+        time += b[i + 1] - b[i];
+    }
+    return time;
+}
+
+int main() {
+    int N;
+    int Nlines;
+    cin >> N;
+    cin >> Nlines;
+    vector<vector<squirrel_t>> squirrels(N);
+    for (int i = 0; i < Nlines; i++) {
+        squirrel_t squirrel;
+        cin >> squirrel.C;
+        string s;
+        cin >> s;
+        squirrel.start = true;
+        if (s == "STOP") {
+            squirrel.start = false;
+        }
+        cin >> squirrel.HH;
+        cin >> squirrel.MM;
+        squirrels[squirrel.C - 1].push_back(squirrel);
+    }
+    for (auto &i : squirrels) {
+        sort(i.begin(), i.end());
+        int ct = cleaning_time(i);
+        cout << ct / 60 << " " << ct % 60 << "\n";
+    }
+}
