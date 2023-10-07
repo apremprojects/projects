b6ca083c6ab64064a7b17ec657e2d0a48f727caa
apremprojects <arjundprem@outlook.com>
2023-10-06 19:51:51 -0700

    Added Radar Brands, Out of Place, and Election Time

---
+++ usaco/Election Time/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <array>
+#include <set>
+#include <fstream>
+using namespace std;
+
+vector<int> shuffler(vector<int> ids, const vector<int> &shuffle) {
+    vector<int> result(ids.size());
+    for (int i = 0; i < ids.size(); i++) {
+        //result[shuffle[i] - 1] = ids[i];
+        result[i] = ids[shuffle[i] - 1];
+    }
+    return result;
+}
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> shuffle(N);
+    vector<int> ids(N);
+    for (int i = 0; i < N; i++) {
+        cin >> shuffle[i];
+    }
+    for (int i = 0; i < N; i++) {
+        cin >> ids[i];
+    }
+    for (int i = 0; i < 3; i++) {
+        ids = shuffler(ids, shuffle);
+    }
+    for (auto i : ids) {
+        cout << i << "\n";
+    }
+}
---
+++ usaco/Radar Brands/main.cpp
@@ -0,0 +1,44 @@
+﻿#include <string>
+#include <vector>
+#include <array>
+#include <fstream>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int min_index(const vector<int>& nums, const int u_start) {
+	int min = nums[u_start];
+	int min_ind = u_start;
+	for (int i = u_start; i < nums.size(); i++) {
+		if (nums[i] < min) {
+			min = nums[i];
+			min_ind = i;
+		}
+	}
+	return min_ind;
+}
+
+int special_sort(vector<int>& nums, vector<pair<int, int>> &ps) {
+	int count = 0;
+	for (int u_start = 0; u_start < nums.size(); u_start++) {
+		int min_ind = min_index(nums, u_start);
+		if (min_ind != u_start) {
+			swap(nums[min_ind], nums[u_start]);
+			cout << u_start << " " << min_ind << "\n";
+			count++;
+		}
+	}
+	return count;
+}
+
+int main() {
+	int N;
+	cin >> N;
+	vector<int> nums(N);
+	vector<pair<int, int>> ps(N, pair<int, int>(0, 0));
+	for (int i = 0; i < N; i++) {
+		cin >> nums[i];
+	}
+	cout << special_sort(nums, ps);
+}
