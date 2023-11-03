6a5582bbf9ac2f27fa0e36ff76fa7b315c88e830
Arjun.D.Prem <arjundprem@outlook.com>
2023-11-03 13:56:15 -0900

    Finalized Romi Robot Joystick controller program

--- expedition/romi_robot/romi_robot_with_controller/work_thread.cpp
+++ expedition/romi_robot/romi_robot_with_controller/work_thread.cpp
@@ -1,37 +1,57 @@
 #include "work_thread.h"
+#include "romi_robot.h"
 #include <QDebug>
 #include <QMutexLocker>
 
-WorkThread::WorkThread(const QString& port_name, QObject* parent)
-	: QThread(parent),
-    port_name(port_name) {}
+WorkThread::WorkThread(const QString& port_name, QObject* parent) : QThread(parent), robot(port_name) {}
 
 void WorkThread::run() {
-	RomiRobot robot(port_name, this);
-	QPair<int, int> item;
+	robot.setGains(600, 110000, 100, 25);
+	QPair<double, double> item;
+	auto prev = std::chrono::high_resolution_clock::now();
+	auto prev_two = std::chrono::high_resolution_clock::now();
+	robot.setSetpoints(MotionCmd(0,0,10));
 	while (running) {
-		{
-			const QMutexLocker locker(&mutex);
-			if (queue.empty()) {
-				cond_wait.wait(&mutex);
-				continue;
-			}
-			item = queue.dequeue();
-			mutex.unlock();
+		//qDebug() << "Here..";
+		auto curr = std::chrono::high_resolution_clock::now();
+		auto timediff = std::chrono::duration_cast<std::chrono::milliseconds>(curr - prev);
+		//qDebug() << "Here2..";
+		if(timediff.count() > 250){
+			robot.sendHeartbeat();
+			prev = curr;
 		}
-		const auto motor_state = robot.set_motors(item.first, item.second);
+		//qDebug() << "Here3..";
+		auto status = robot.readStatus();
+		//qDebug() << "Robot status -> " << status.queue_size << queue.size();
+		mutex.lock();/*
+		if (queue.empty()) {
+			cond_wait.wait(&mutex);
+			continue;
+		}*/
+		auto curr_two = std::chrono::high_resolution_clock::now();
+		auto timediff_two = std::chrono::duration_cast<std::chrono::milliseconds>(curr_two - prev_two);
+		if(status.queue_size < 4 && !queue.empty() && timediff_two.count() > 30){
+			item = queue.dequeue();
+			int left_rpm = item.first * max_velocity + item.second * max_yaw_velocity;
+			int right_rpm = item.first * max_velocity - item.second * max_yaw_velocity;
+			//qDebug() << "Running setSetpoints...";
+			robot.setSetpoints(MotionCmd(left_rpm, right_rpm, 1));
+			//qDebug() << "Done running setSetpoints...";
+			prev_two = curr_two;
+		}
+		mutex.unlock();
 	}
 }
 
-void WorkThread::sendCommand(const int left, const int right) {
+void WorkThread::sendCommand(const double throttle, const double yaw_strength) {
 	{
 		const QMutexLocker locker(&mutex);
-		queue.append(QPair<int, int>(left, right));
+		queue.append(QPair<double, double>(throttle, yaw_strength));
 	}
-	cond_wait.wakeAll();
+	//cond_wait.wakeAll();
 }
 
 void WorkThread::stop() {
 	running = false;
-	cond_wait.wakeAll();
+	//cond_wait.wakeAll();
 }
